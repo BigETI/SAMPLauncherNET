709fe7b72dc22bade6979d9f5c1170bb23cba2ca
BigETI <bigeti.ethem@gmail.com>
2017-09-27 21:42:13 +0200

    Changed russian version. Changed assembly, file and product version. Updated update.json

--- SAMPLauncherNET/Properties/AssemblyInfo.cs
+++ SAMPLauncherNET/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("SAMPLauncherNET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SAMPLauncherNET is a launcher for San Andreas Multiplayer")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SAMPLauncherNET")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.3.1")]
+[assembly: AssemblyFileVersion("1.0.3.1")]
