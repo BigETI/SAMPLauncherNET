5ed56bf3460433f3ab61e663a58db556d01c34ac
Megadragon <ssglobov@gmail.com>
2019-05-30 17:04:05 +0300

    Fix language code uk-UK to uk-UA

--- SAMPLauncherNET/Languages/uk-UA.Designer.cs
+++ SAMPLauncherNET/Languages/uk-UA.Designer.cs
@@ -22,14 +22,14 @@ namespace SAMPLauncherNET.Languages {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class uk_UK {
+    internal class uk_UA {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal uk_UK() {
+        internal uk_UA() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace SAMPLauncherNET.Languages {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SAMPLauncherNET.Languages.uk-UK", typeof(uk_UK).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SAMPLauncherNET.Languages.uk-UA", typeof(uk_UA).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
